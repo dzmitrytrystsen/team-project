d7d650102d06a92b89d57dc2eb483267e6c0de20
Victor Yaloshka <jayeti@mail.ru>
2021-05-14 20:37:24 +0300

    test commit

--- Assets/Scripts/EnemySpawner/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner/EnemySpawner.cs
@@ -1,11 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum WaveEnemyType
+{
+    Small,
+    Middle,
+    Big
+}
+
+[System.Serializable]
+public struct Wave
+{
+    public WaveEnemyType WaveEnemyType;
+    public int AmountOfEnemies;
+}
+
 public class EnemySpawner : MonoBehaviour
 {
     [Header("Enemy Settings")]
+<<<<<<< Updated upstream
     [SerializeField] private GameObject _enemyPrefab;
+=======
+    [SerializeField] private GeneralEnemy _enemyPrefab;
+    [SerializeField] private List<Enemy> _enemies;
+>>>>>>> Stashed changes
 
     [Header("Spawn Settings")]
     [SerializeField] private Transform _enemyStartPosition;
@@ -15,24 +35,47 @@ public class EnemySpawner : MonoBehaviour
     public delegate void ReturnEnemyAction();
     public event ReturnEnemyAction OnEnemyReturnToThePool;
 
+<<<<<<< Updated upstream
     private bool _isLevelRunning;
 
     private List<GameObject> _enemiesToPool = new List<GameObject>();
+=======
+    public delegate void EnemyIsKilledAction();
+    public event EnemyIsKilledAction OnEnemyWasKilled;
+
+    private float _timeBetweenWaves;
+    private int _currentWave;
+    private bool _isCurrentWaveSpawing;
+    [SerializeField] private bool _areAnyWavesToSpawnLeft;
+
+    private List<Wave> _wavesNeedToBeSpawned;
+>>>>>>> Stashed changes
+
+    private Dictionary<WaveEnemyType, List<GeneralEnemy>> _enemiesToPool = new Dictionary<WaveEnemyType, List<GeneralEnemy>>();
+
+    [System.Serializable]
+    private struct Enemy
+    {
+        public WaveEnemyType EnemyType;
+        public GeneralEnemy EnemyPrefab;
+    }
 
     private void Awake()
     {
-        _isLevelRunning = true;
-
-        GeneratePoolEnemies(_amountToPool, _enemyPrefab, _enemiesToPool);
+        GeneratePoolListsForEnemies(_enemies);
     }
 
     private void Start()
     {
-        StartCoroutine(SpawnEnemyInTime());
+        _currentWave = 0;
+        _areAnyWavesToSpawnLeft = true;
+
+        StartCoroutine(SpawnNextWave(_currentWave));
     }
 
-    private IEnumerator SpawnEnemyInTime()
+    public void WavesNeedToBeSpawned(List<Wave> wavesNeedToBeSpawned, float timeBetweenWaves)
     {
+<<<<<<< Updated upstream
         while (_isLevelRunning)
         {
             GameObject enemyFromPool = GetEnemyFromPool(_enemiesToPool);
@@ -41,34 +84,92 @@ public class EnemySpawner : MonoBehaviour
 
             yield return new WaitForSeconds(_timeBetweenSpawn);
         }
+=======
+        _wavesNeedToBeSpawned = wavesNeedToBeSpawned;
+        _timeBetweenWaves = timeBetweenWaves;
+>>>>>>> Stashed changes
     }
 
-    private void OnDestroy()
+    private void GeneratePoolListsForEnemies(List<Enemy> enemies)
     {
+<<<<<<< Updated upstream
         foreach (GameObject enemy in _enemiesToPool)
+=======
+        foreach (Enemy enemy in enemies)
+>>>>>>> Stashed changes
         {
-            enemy.GetComponent<GeneralEnemy>().OnReadyToReturnToThePool -= ReturnEnemyToThePool;
-            enemy.GetComponent<GeneralEnemy>().OnDamageWasTaken -= TryToReturnEnemyToThePool;
-        }
-    }
+            _enemiesToPool.Add(enemy.EnemyType, new List<GeneralEnemy>());
 
-    private GameObject GetEnemyFromPool(List<GameObject> listToPoolFrom)
-    {
-        for (int i = 0; i < listToPoolFrom.Count; i++)
-        {
-            if (!listToPoolFrom[i].activeInHierarchy)
+            for (int i = 0; i < _amountToPool; i++)
             {
-                return listToPoolFrom[i];
+                GeneralEnemy enemyObject = Instantiate(enemy.EnemyPrefab, transform.position, Quaternion.identity);
+                enemyObject.gameObject.transform.SetParent(transform);
+
+                // Subscribe to the Enemy events
+                enemyObject.OnReadyToReturnToThePool += ReturnEnemyToThePool;
+                enemyObject.OnDamageWasTaken += TryToReturnEnemyToThePool;
+
+                enemyObject.gameObject.SetActive(false);
+                _enemiesToPool[enemy.EnemyType].Add(enemyObject);
             }
         }
-
-        return null;
     }
 
-    private void GeneratePoolEnemies(int amountToPool, GameObject enemyPrefab, List<GameObject> listToAdd)
+<<<<<<< Updated upstream
+    private GameObject GetEnemyFromPool(List<GameObject> listToPoolFrom)
+=======
+    private IEnumerator SpawnNextWave(int waveToSpawnIndex)
+>>>>>>> Stashed changes
     {
-        for (int i = 0; i < amountToPool; i++)
+        if (_areAnyWavesToSpawnLeft)
         {
+<<<<<<< Updated upstream
+            if (!listToPoolFrom[i].activeInHierarchy)
+=======
+            Wave waveNeesToBeSpawned = _wavesNeedToBeSpawned[waveToSpawnIndex];
+
+            for (int i = 0; i < waveNeesToBeSpawned.AmountOfEnemies; i++)
+>>>>>>> Stashed changes
+            {
+                _isCurrentWaveSpawing = true;
+                SpawnEnemy(waveNeesToBeSpawned.WaveEnemyType);
+                yield return new WaitForSeconds(_timeBetweenSpawn);
+            }
+
+            _currentWave++;
+            if (waveToSpawnIndex < _wavesNeedToBeSpawned.Count - 1)
+            {
+                _isCurrentWaveSpawing = false;
+                StartCoroutine(WaitAndSpawnNextWave());
+            }
+            else
+                _areAnyWavesToSpawnLeft = false;
+        }
+    }
+
+    private IEnumerator WaitAndSpawnNextWave()
+    {
+        yield return new WaitForSeconds(_timeBetweenWaves);
+        
+        StartCoroutine(SpawnNextWave(_currentWave));
+    }
+
+    private void SpawnEnemy(WaveEnemyType enemyTypeToSpawn)
+    {
+        GeneralEnemy enemyFromPool = GetEnemyFromPool(enemyTypeToSpawn);
+        enemyFromPool.transform.position = _enemyStartPosition.position;
+        enemyFromPool.gameObject.SetActive(true);
+    }
+
+<<<<<<< Updated upstream
+    private void GeneratePoolEnemies(int amountToPool, GameObject enemyPrefab, List<GameObject> listToAdd)
+=======
+    private void OnDestroy()
+>>>>>>> Stashed changes
+    {
+        foreach (List<GeneralEnemy> enemiesList in _enemiesToPool.Values)
+        {
+<<<<<<< Updated upstream
             GameObject gameObject = Instantiate<GameObject>(enemyPrefab, transform.position, Quaternion.identity);
             gameObject.transform.SetParent(transform);
 
@@ -78,7 +179,29 @@ public class EnemySpawner : MonoBehaviour
 
             gameObject.SetActive(false);
             listToAdd.Add(gameObject);
+=======
+            foreach (GeneralEnemy enemy in enemiesList)
+            {
+                enemy.OnReadyToReturnToThePool -= ReturnEnemyToThePool;
+                enemy.OnDamageWasTaken -= TryToReturnEnemyToThePool;
+            }
         }
+    }
+
+    private GeneralEnemy GetEnemyFromPool(WaveEnemyType enemyTypeToLookFor)
+    {
+        List<GeneralEnemy> listToPoolFrom = _enemiesToPool[enemyTypeToLookFor];
+
+        for (int i = 0; i < listToPoolFrom.Count; i++)
+        {
+            if (!listToPoolFrom[i].gameObject.activeInHierarchy)
+            {
+                return listToPoolFrom[i];
+            }
+>>>>>>> Stashed changes
+        }
+
+        return null;
     }
 
     private void TryToReturnEnemyToThePool(int enemyHealthLeft, GameObject enemyGameObject)
@@ -89,9 +212,31 @@ public class EnemySpawner : MonoBehaviour
             return;
     }
 
+    private bool IfAnyEnemyIsActive()
+    {
+        foreach (List<GeneralEnemy> enemiesList in _enemiesToPool.Values)
+        {
+            foreach (GeneralEnemy enemy in enemiesList)
+            {
+                if (enemy.gameObject.activeInHierarchy)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
     private void ReturnEnemyToThePool(GameObject enemyGameObject)
     {
         enemyGameObject.SetActive(false);
         OnEnemyReturnToThePool?.Invoke();
+
+        if (!_isCurrentWaveSpawing && _areAnyWavesToSpawnLeft
+            && !IfAnyEnemyIsActive())
+        {
+            StartCoroutine(SpawnNextWave(_currentWave));
+        }
     }
 }
--- Assets/Scripts/LevelManager/LevelManager.cs
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,22 +6,46 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour
 {
     [Header("Level Settings")]
-    [SerializeField] int _enemiesNeedToBeKilled;
+    [SerializeField] float _timeBetweenWaves;
+    [SerializeField] private List<Wave> _waves;
 
     [SerializeField] private EnemySpawner _enemySpawner;
 
     private int _enemiesKilled;
+    private int _enemiesNeedToBeKilled;
 
     private void Awake()
     {
         _enemiesKilled = 0;
+        _enemiesNeedToBeKilled = CountAllEnemies(_waves);
 
+<<<<<<< Updated upstream
         _enemySpawner.OnEnemyReturnToThePool += TryToCompleteLevel;
+=======
+        _enemySpawner.OnEnemyWasKilled += TryToCompleteLevel;
+
+        if (_waves.Count < 1)
+            throw new Exception("No Waves are set in the " + name);
+        else
+            _enemySpawner.WavesNeedToBeSpawned(_waves, _timeBetweenWaves);
+>>>>>>> Stashed changes
     }
 
     private void Start()
     {
         FindObjectOfType<PlayerBase>().OnDamageWasTaken += TryToFinishTheGame;
+    }
+
+    private int CountAllEnemies(List<Wave> wavesList)
+    {
+        int enemiesCount = 0;
+
+        foreach (Wave wave in wavesList)
+        {
+            enemiesCount += wave.AmountOfEnemies;
+        }
+
+        return enemiesCount;
     }
 
     private void TryToCompleteLevel()
